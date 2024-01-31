aad3fc889f350d37062751faf5d8a86187b2eec1
jaybyrd3 <123055625+jaybyrd3@users.noreply.github.com>
2024-01-31 17:10:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/jay')
+def jay():
+    return render_template('jay.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
