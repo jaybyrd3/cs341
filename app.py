1aadb86f5caa38b683b5423ea369b8dc2dcd6a2d
jaybyrd3 <123055625+jaybyrd3@users.noreply.github.com>
2024-02-07 12:36:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
