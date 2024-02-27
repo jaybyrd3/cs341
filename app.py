2e347a6b15cde8ef03352b6e1fd521517d95897c
Tarren Yauchler <tarrenkirk@gmail.com>
2024-02-27 16:30:34 -0600

    (signup fix) wasn't flash messages, was likely my sql query (hopefully this fixes it)

--- app.py
+++ app.py
@@ -40,20 +40,20 @@ def signup():
 		confirm = request.form['confirm']
 		if password == confirm:
 			# query db to see if user exists
-			user = User.query.filter(User.email == email).all()
+			user = User.query.filter_by(email = email).all()
 			if not user:  # Check if user is an empty list
 				# we know they're new & can add them
 				new_user = User(email=email, username=email, password=password)
 				db.session.add(new_user)
 				db.session.commit()
-				#flash(f"You have successfully made an account under the email {email}!", category="success")
+				flash(f"You have successfully made an account under the email {email}!", category="success")
 				return render_template('signup.html')
 			else:
 				# we know the user already exists
-				#flash(f"There is already an account registered under the email {email}. Please log in to continue.")
+				flash(f"There is already an account registered under the email {email}. Please log in to continue.")
 				return render_template('signup.html')
 		else:
-			#flash(f"Your entered passwords do not match.", category="error")
+			flash(f"Your entered passwords do not match.", category="error")
 			return render_template('signup.html')
 	return render_template('signup.html')
     
