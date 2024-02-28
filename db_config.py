2dcded43ed87c32cda036c3559b38e11e3537ef5
Tarren Yauchler <tarrenkirk@gmail.com>
2024-02-28 17:12:00 -0600

    Added firstName, lastName, jobTitle, and qualifications fields to User table (all nullable & non-unique)

--- db_config.py
+++ db_config.py
@@ -13,6 +13,10 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=True, nullable=False)
+    firstName = db.Column(db.String(80), unique=False, nullable=True)
+    lastName = db.Column(db.String(80), unique=False, nullable=True)
+    jobTitle = db.Column(db.String(80), unique=False, nullable=True)
+    qualifications = db.Column(db.String(80), unique=False, nullable=False)
     appointments = db.relationship('Slot', backref='theUser', lazy=True) # is a 1-to-many relationship by default
     slots = db.relationship('Slot', backref='theProvider', lazy=True)
 
