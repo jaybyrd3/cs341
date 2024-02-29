34daf75c21b658306be036b6473a6dea1096d991
jaybyrd3 <123055625+jaybyrd3@users.noreply.github.com>
2024-02-28 21:43:00 -0600

    Update app.py
    trying something new with user.get

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 @login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+def load_user(id):
+    return User.query.get(id)
 
 #initialize secret key (we'll change this later)
 
