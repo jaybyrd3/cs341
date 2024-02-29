b1a4c86a4a9e4a7dc59dd6bc666bbe369fa679ea
jaybyrd3 <123055625+jaybyrd3@users.noreply.github.com>
2024-02-28 20:25:17 -0600

    Update db_config.py
    nullable!!

--- db_config.py
+++ db_config.py
@@ -11,13 +11,13 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=False, nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    password = db.Column(db.String(80), unique=False, nullable=True)
     firstName = db.Column(db.String(80), unique=False, nullable=True)
     lastName = db.Column(db.String(80), unique=False, nullable=True)
     jobTitle = db.Column(db.String(80), unique=False, nullable=True)
-    qualifications = db.Column(db.String(80), unique=False, nullable=False)
+    qualifications = db.Column(db.String(80), unique=False, nullable=True)
     # appointments = db.relationship('Slot', backref='user', lazy=True, foreign_keys=[Slot.user_id]) # is a 1-to-many relationship by default
     slots = db.relationship('Slot', backref='slot', lazy=True)
 
@@ -27,7 +27,7 @@ class User(db.Model):
 class Slot(db.Model):
     __tablename__ = 'slot'
     id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime, unique=True, nullable=False)
+    time = db.Column(db.DateTime, unique=True, nullable=True)
     # if user_id == null, then appt slot is open
     client = db.Column(db.Text, db.ForeignKey('user.email'), nullable=True)
     # slot will always have a provider
