415ea28fc627bceb870873024e7b42557fe34a07
jaybyrd3 <123055625+jaybyrd3@users.noreply.github.com>
2024-02-28 20:09:26 -0600

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -10,27 +10,29 @@ db = SQLAlchemy()
 
 # Define your models here
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
     firstName = db.Column(db.String(80), unique=False, nullable=True)
     lastName = db.Column(db.String(80), unique=False, nullable=True)
     jobTitle = db.Column(db.String(80), unique=False, nullable=True)
     qualifications = db.Column(db.String(80), unique=False, nullable=False)
-    appointments = db.relationship('Slot', backref='user', lazy=True, foreign_keys=[Slot.user_id]) # is a 1-to-many relationship by default
-    slots = db.relationship('Slot', backref='user', lazy=True, foreign_keys=[Slot.provider_id])
+    # appointments = db.relationship('Slot', backref='user', lazy=True, foreign_keys=[Slot.user_id]) # is a 1-to-many relationship by default
+    slots = db.relationship('Slot', backref='slot', lazy=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
 
 class Slot(db.Model):
+    __tablename__ = 'slot'
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.DateTime, unique=True, nullable=False)
     # if user_id == null, then appt slot is open
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    client = db.Column(db.Text, db.ForeignKey('user.email'), nullable=True)
     # slot will always have a provider
-    provider_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # provider_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
     def __repr__(self):
 	    return f'<Slot {self.time}>'
