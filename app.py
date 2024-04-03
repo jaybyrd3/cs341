a2fac4f99b5dd606b0970a5af7911e346bad5ee2
jaybyrd3 <123055625+jaybyrd3@users.noreply.github.com>
2024-04-03 01:21:19 -0500

    commented out debug prints that were causing an error with indentation...

--- app.py
+++ app.py
@@ -94,10 +94,11 @@ def booknew():
         slot_id = request.form.get('slot_id')
         slot = Slot.query.get(slot_id)
 	# debug stuff
-        print(f"slot_id {slot_id} slot {slot}")
+        # print(f"slot_id {slot_id} slot {slot}")
         if slot: #and not slot.client
             slot.client = session.get('email')  # Or however you identify the client
-    	    print(f"slot.client {session.get('email')}")
+	    #debug stuff
+    	    # print(f"slot.client {session.get('email')}")
             db.session.commit()
             flash('Appointment booked successfully!', 'success')
             return redirect(url_for('viewappointments'))
