79bddbc757713c4103bce6e0bdae036585495b00
jaybyrd3 <123055625+jaybyrd3@users.noreply.github.com>
2024-04-03 13:14:49 -0500

    Update db_config.py adding is_admin

--- db_config.py
+++ db_config.py
@@ -14,6 +14,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     is_active = db.Column(db.Boolean, default=True)
+    is_admin = db.Column(db.Boolean, default=False)
     username = db.Column(db.String(80), unique=False, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=True)
     password_hash = db.Column(db.String(256), unique=False, nullable=True)
