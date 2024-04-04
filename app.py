0692ec9755c85a46e24e63bc8bd133484a492d0e
Tarren Yauchler <tarrenkirk@gmail.com>
2024-04-03 19:39:33 -0500

    more debugging (i wanna perish)

--- app.py
+++ app.py
@@ -152,26 +152,18 @@ def booknewcat(category):
                 ~Slot.id.in_(
                     select(Slot.id)
                     .where(
-                        func.any_(
-                            Slot.starttime >= tup[0] for tup in occupied_ranges
-                        )
-                        and func.any_(
-                            Slot.starttime <= tup[1] for tup in occupied_ranges
-                        )
-                    ).execution_options(autocommit=True)  # Add debugging prints outside
+                        func.any_(Slot.starttime >= tup[0] for tup in occupied_ranges)
+                        and func.any_(Slot.starttime <= tup[1] for tup in occupied_ranges)
+                    )
                 )
             )
             .filter(
                 ~Slot.id.in_(
                     select(Slot.id)
                     .where(
-                        func.any_(
-                            Slot.endtime >= tup[0] for tup in occupied_ranges
-                        )
-                        and func.any_(
-                            Slot.endtime <= tup[1] for tup in occupied_ranges
-                        )
-                    ).execution_options(autocommit=True)  # Add debugging prints outside
+                        func.any_(Slot.endtime >= tup[0] for tup in occupied_ranges)
+                        and func.any_(Slot.endtime <= tup[1] for tup in occupied_ranges)
+                    )
                 )
             )
         )
